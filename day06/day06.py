1f3b6cf2a0a55fe17f1a76ec2c4f26befb2fca3b
Bob Follek <bfollek@gmail.com>
2019-05-31 10:04:05 -0400

    Fixed README, started day06

---
+++ day06/day06.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+def part1(file_name):
+    """
+    """
+    pass
+
+
+def part2(file_name):
+    """
+    """
+    pass
---
+++ day06/test_day06.py
@@ -0,0 +1,9 @@
+from day06 import part1, part2
+
+
+def test_part1():
+    assert part1("day06/day06.txt") == "the right answer"
+
+
+def test_part2():
+    assert part2("day06/day06.txt") == "the next right answer"
