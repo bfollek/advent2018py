1780b5368672cde745cda549bf7dab084caa886c
Bob Follek <bfollek@gmail.com>
2019-06-06 21:55:21 -0400

    Started day07

---
+++ day07/test_day07.py
@@ -0,0 +1,9 @@
+from day07 import part1, part2
+
+
+def test_part1():
+    assert part1("day07/day07.txt") == "HEGMPOAWBFCDITVXYZRKUQNSLJ"
+
+
+def test_part2():
+    assert part2("day07/day07.txt") == 2
