3e0a7ba207c0b14f18588b748466d571e4352de7
Bob Follek <bfollek@gmail.com>
2019-06-14 05:38:27 -0400

    WIP

--- day07/step.py
+++ day07/step.py
@@ -35,10 +35,12 @@ class Step:
 class TimedStep(Step):
     def start(self, time_now):
         """
-        Each step takes 60 seconds plus an amount corresponding to its letter: A=1, B=2, C=3, and so on.
-        So, step A takes 60+1=61 seconds, while step Z takes 60+26=86 seconds. No time is required between steps.
+        time_now is an integer.
+
+        'Each step takes 60 seconds plus an amount corresponding to its letter: A=1, B=2, C=3, and so on.
+        So, step A takes 60+1=61 seconds, while step Z takes 60+26=86 seconds. No time is required between steps.'
         """
-        self._completion_time = ord(self.name) - ord("A") + 61 + time_now
+        self._time_complete = ord(self.name) - ord("A") + 61 + time_now
 
     def completed(self, time_now):
-        return self._completion_time <= time_now
+        return self._time_complete <= time_now
