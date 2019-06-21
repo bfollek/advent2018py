37eabeb90627acb4821cc51104081b070eba3e8b
Bob Follek <bfollek@gmail.com>
2019-06-20 20:58:14 -0400

    Fixed comment linebreaks ATP

--- day08/node.py
+++ day08/node.py
@@ -30,7 +30,9 @@ class Node:
         """
         If a node has no child nodes, its value is the sum of its metadata entries.
 
-        However, if a node does have child nodes, the metadata entries become indexes which refer to those child nodes. A metadata entry of 1 refers to the first child node, 2 to the second, 3 to the third, and so on. The value of this node is the sum of the values of the child nodes referenced by the metadata entries. If a referenced child node does not exist, that reference is skipped. A child node can be referenced multiple time and counts each time it is referenced. A metadata entry of 0 does not refer to any child node.
+        However, if a node does have child nodes, the metadata entries become indexes
+        which refer to those child nodes. The value of this node is the sum of the
+        values of the child nodes referenced by the metadata entries.
         """
         if not self.children:
             return sum(self.metadata)
@@ -39,7 +41,11 @@ class Node:
 
     def _valid_metachildren(self):
         """
-        A metadata entry of 1 refers to the first child node, 2 to the second, 3 to the third, and so on... If a referenced child node does not exist, that reference is skipped. A child node can be referenced multiple time and counts each time it is referenced. A metadata entry of 0 does not refer to any child node.
+        A metadata entry of 1 refers to the first child node, 2 to the second, 3 to
+        the third, and so on. If a referenced child node does not exist, that
+        reference is skipped. A child node can be referenced multiple times and
+        counts each time it is referenced. A metadata entry of 0 does not refer to
+        any child node.
         """
         valid = []
         for i in self.metadata:
