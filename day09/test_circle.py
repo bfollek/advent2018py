96db650d2cfb13809f34c1e080dccc152aeb68a0
Bob Follek <bfollek@gmail.com>
2019-07-10 11:35:52 -0400

    Refactored place tests ATP

--- day09/test_circle.py
+++ day09/test_circle.py
@@ -1,5 +1,42 @@
 from circle import Circle
 
+PLACE_TESTS = {
+    #  (_current, _list)
+    1: (1, [0, 1]),
+    2: (1, [0, 2, 1]),
+    3: (3, [0, 2, 1, 3]),
+    4: (1, [0, 4, 2, 1, 3]),
+    5: (3, [0, 4, 2, 5, 1, 3]),
+    22: (
+        13,
+        [
+            0,
+            16,
+            8,
+            17,
+            4,
+            18,
+            9,
+            19,
+            2,
+            20,
+            10,
+            21,
+            5,
+            22,
+            11,
+            1,
+            12,
+            6,
+            13,
+            3,
+            14,
+            7,
+            15,
+        ],
+    ),
+}
+
 
 def test_init():
     c = Circle()
@@ -7,46 +44,11 @@ def test_init():
     assert c._current == 0
 
 
-def test_place_1():
-    c = Circle()
-    c.place(1)
-    assert c._list == [0, 1]
-    assert c._current == 1
-
-
-def test_place_2():
-    c = Circle()
-    c.place(1)
-    c.place(2)
-    assert c._list == [0, 2, 1]
-    assert c._current == 1
-
-
-def test_place_3():
-    c = Circle()
-    c.place(1)
-    c.place(2)
-    c.place(3)
-    assert c._list == [0, 2, 1, 3]
-    assert c._current == 3
-
-
-def test_place_4():
-    c = Circle()
-    c.place(1)
-    c.place(2)
-    c.place(3)
-    c.place(4)
-    assert c._list == [0, 4, 2, 1, 3]
-    assert c._current == 1
-
-
-def test_place_5():
-    c = Circle()
-    c.place(1)
-    c.place(2)
-    c.place(3)
-    c.place(4)
-    c.place(5)
-    assert c._list == [0, 4, 2, 5, 1, 3]
-    assert c._current == 3
+def test_place():
+    for input, result in PLACE_TESTS.items():
+        c = Circle()
+        for i in range(1, input + 1):
+            c.place(i)
+        current, list = result
+        assert c._current == current
+        assert c._list == list
