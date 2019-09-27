ede620925781e8192fd29861f0de902409b79b4d
Bob Follek <bfollek@gmail.com>
2019-09-27 11:59:11 -0400

    day11 _cell_power_level isworking ATP

--- day11/day11.py
+++ day11/day11.py
@@ -15,24 +15,6 @@ def _cell_power_level(grid_serial_num, x, y: int) -> int:
     """
     Calculate the power level of a fuel cell.
     """
-    # For example, to find the power level of the fuel cell at 3,5 in a grid with serial number 8:
-
-    #     The rack ID is 3 + 10 = 13.
-    #     The power level starts at 13 * 5 = 65.
-    #     Adding the serial number produces 65 + 8 = 73.
-    #     Multiplying by the rack ID produces 73 * 13 = 949.
-    #     The hundreds digit of 949 is 9.
-    #     Subtracting 5 produces 9 - 5 = 4.
-
-    # So, the power level of this fuel cell is 4.
-
-    # Here are some more example power levels:
-
-    #     Fuel cell at  122,79, grid serial number 57: power level -5.
-    #     Fuel cell at 217,196, grid serial number 39: power level  0.
-    #     Fuel cell at 101,153, grid serial number 71: power level  4.
-    # Find the fuel cell's rack ID, which is its X coordinate plus 10.
-
     rack_id = x + 10
     # Begin with a power level of the rack ID times the Y coordinate.
     power_level = rack_id * y
